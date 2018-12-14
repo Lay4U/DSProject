5063a8bf03cd2240561f72a5c75d9836111ae172
Lay4U <kmr345@naver.com>
2018-12-15 08:20:33 +0900

    get db ver2

--- src/PreProcessing.java
+++ src/PreProcessing.java
@@ -13,62 +13,62 @@ import org.jsoup.safety.Whitelist;
 import java.io.*;
 
 public class PreProcessing {
-	public String[] result;
+    public String[] result;
 
-	public static void main(String[] args) throws Exception {
-		PrintWriter out = new PrintWriter("filename.txt");
+    public static void main(String[] args) throws Exception {
+        PrintWriter out = new PrintWriter("filename.txt");
 
-		URL url = new URL("https://en.wikipedia.org/wiki/Distributed_computing");
-		BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+        URL url = new URL("https://en.wikipedia.org/wiki/Distributed_computing");
+        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
 
-		String inputLine = "";
-		String input = "";
+        String inputLine = "";
+        String input = "";
 
-		while ((inputLine = in.readLine()) != null)
-			input += inputLine;
+        while ((inputLine = in.readLine()) != null)
+            input += inputLine;
 
-		Document jsoupDoc = Jsoup.parse(input);
-		int count = Jsoup.parse(input).text().split(" ").length;
+        Document jsoupDoc = Jsoup.parse(input);
+        int count = Jsoup.parse(input).text().split(" ").length;
 
-		String temp = jsoupDoc.text().replaceAll("\\.", "").replaceAll("\"", "").replaceAll(",", "").replaceAll(":", "")
-				.replaceAll("\\(", "").replaceAll("\\)", "").replaceAll(";", "").replaceAll("\\[(.*?)\\]", "")
-				.replaceAll("®", "").replaceAll("-", "");
-		
-		
+        String temp = jsoupDoc.text().replaceAll("\\.", "").replaceAll("\"", "").replaceAll(",", "").replaceAll(":", "")
+                .replaceAll("\\(", "").replaceAll("\\)", "").replaceAll(";", "").replaceAll("\\[(.*?)\\]", "")
+                .replaceAll("®", "").replaceAll("-", "");
+        
+        
 
-		System.out.println("total word count :" + count);
-		Connection connection = null;
-		Statement st = null;
-		ResultSet rs = null;
-		
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		connection = DriverManager.getConnection(
-				"jdbc:mysql://localhost:3306/ds?characterEncoding=UTF-8&serverTimezone=UTC", "root", "kgu123");
-		st = connection.createStatement();
+        System.out.println("total word count :" + count);
+        Connection connection = null;
+        Statement st = null;
+        ResultSet rs = null;
+        
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/ds?characterEncoding=UTF-8&serverTimezone=UTC", "root", "kgu123");
+        st = connection.createStatement();
 
-//sql 삽입		
-//		String sql;
-//		for (String word : result) {
-//			sql = "INSERT INTO CONTENT(word)" + " VALUE (\"" + word + "\");";
-//			st.execute(sql);
-//		}
-		
-		
-//		System.out.println("finised insert value");
-//		String sql = "SELECT COUNT(word), word FROM content GROUP BY word ORDER BY word;";
-//		rs = st.executeQuery(sql);
-//		System.out.println(rs);
-		
-		int rowCnt = 0;
+//sql 삽입        
+//        String sql;
+//        for (String word : result) {
+//            sql = "INSERT INTO CONTENT(word)" + " VALUE (\"" + word + "\");";
+//            st.execute(sql);
+//        }
+        
+        
+//        System.out.println("finised insert value");
+//        String sql = "SELECT COUNT(word), word FROM content GROUP BY word ORDER BY word;";
+//        rs = st.executeQuery(sql);
+//        System.out.println(rs);
+        
+        int rowCnt = 0;
         rs = st.executeQuery("SELECT COUNT(*) FROM content");
         if(rs.next()) rowCnt = rs.getInt(1);
         System.out.println("Total counts : " + rowCnt);
 
 
-		st.close();
-		connection.close();
-		in.close();
-		
-	}
+        st.close();
+        connection.close();
+        in.close();
+        
+    }
 
 }
